4d05475a1d356b6b08def8aa855bf392ba6efa88
yasuaki iwai <iwai@primeservant.co.jp>
2019-10-06 19:39:35 +0900

    計算クラスを作成

---
+++ app/src/main/java/jp/co/primeservant/practiceudemyjenkins/MyCalculator.java
@@ -0,0 +1,8 @@
+package jp.co.primeservant.practiceudemyjenkins;
+
+public class MyCalculator {
+
+    public double calcTriangleSquare(double teihen, double takasa){
+        return teihen * takasa /2;
+    }
+}
