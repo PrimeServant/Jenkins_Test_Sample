6e18e1c5db7dde9d3a7f63ff1fea92ce4d4663a0
yasuaki iwai <iwai@primeservant.co.jp>
2019-10-06 19:48:37 +0900

    円の面積を求めるメソッドを追加

--- app/src/main/java/jp/co/primeservant/practiceudemyjenkins/MyCalculator.java
+++ app/src/main/java/jp/co/primeservant/practiceudemyjenkins/MyCalculator.java
@@ -5,4 +5,8 @@ public class MyCalculator {
     public double calcTriangleSquare(double teihen, double takasa){
         return teihen * takasa /2;
     }
+
+    public double calcCircleSquare(double radius){
+        return radius * radius * 3.14;
+    }
 }
